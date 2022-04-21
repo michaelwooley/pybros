546a47845c9475aab575c72bee7546c1ccd11280
Michael Wooley <wm.wooley@gmail.com>
2022-04-21 19:08:54 -0400

    Add basic wasm setup

---
+++ wasm/src/lib.rs
@@ -0,0 +1,19 @@
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
