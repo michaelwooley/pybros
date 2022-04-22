a8f8417130c209229163a7b5b26f36b8125ee95b
Michael Wooley <wm.wooley@gmail.com>
2022-04-22 03:58:32 -0400

    basic code editor set up

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,12 +3,28 @@
 </script>
 
 <script lang="ts">
-	import PythonExecDemo from '$containers/PythonExecDemo.svelte';
+	import CodeEditor from '$containers/CodeEditor.svelte';
+
+	// import PythonExecDemo from '$containers/PythonExecDemo.svelte';
 </script>
 
 <section class="section">
 	<div class="container">
 		<h1 class="title is-1">pysvelte</h1>
-		<PythonExecDemo />
+
+		<div class="columns has-background-dark is-gapless box p-1 editor-console">
+			<div class="column">
+				<CodeEditor />
+			</div>
+			<div class="column is-narrow">||</div>
+			<div class="column">Thie other</div>
+		</div>
+		<!-- <PythonExecDemo /> -->
 	</div>
 </section>
+
+<style lang="scss">
+	.editor-console {
+		height: 80vh; // 95%
+	}
+</style>
