430c90c321e6c6a5d130a91d1bf1c9f432760a03
Michael Wooley <wm.wooley@gmail.com>
2022-04-22 11:58:01 -0400

    add signalling server for dev

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,17 @@
+import { dev } from '$app/env';
+
 export const PROJECT_NAME = 'pysvelte';
 export const PROJECT_EMOJI = '🥧';
 export const PROJECT_REPO = 'https://github.com/michaelwooley/pysvelte';
 
 export const YJS_INDEXEDDB_EDITOR_KEY = 'y-pysvelte-editor';
 // TODO Add on indexedDb key for other tracked items
+
+// TODO #20 Can we use 3rd party turn servers for webrtc? Kosher?
+export const YJS_WEBRTC_SIGNALLING_SERVERS = dev
+	? ['ws://localhost:4444']
+	: [
+			'wss://signaling.yjs.dev',
+			'wss://y-webrtc-signaling-eu.herokuapp.com',
+			'wss://y-webrtc-signaling-us.herokuapp.com'
+	  ];
