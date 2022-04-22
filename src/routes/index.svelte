db97fd26b14e35b0100b0f0a4b6ebce67946db0e
Michael Wooley <wm.wooley@gmail.com>
2022-04-22 01:30:20 -0400

    Add python textarea

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,53 +3,125 @@
 </script>
 
 <script lang="ts">
+	import { PROJECT_NAME } from '$lib/constants';
+
 	import { onMount } from 'svelte';
 
-	let result: string;
-	let worker: Worker;
+	// let result: string;
+	// let worker: Worker;
+	let pyEval: (source: string, options?: object) => any;
+	let code: string = '';
+	let results: string[] = [];
 	onMount(async () => {
-		const MyWorker = await (await import('$lib/wasmWorker/worker?worker')).default;
+		// const MyWorker = await (await import('$lib/wasmWorker/worker?worker')).default;
 
-		worker = new MyWorker();
-		worker.onmessage = function (event) {
-			console.log(event);
-			console.log(event.data);
-			result = event.data;
-		};
+		// worker = new MyWorker();
+		// worker.onmessage = function (event) {
+		// 	console.log(event);
+		// 	console.log(event.data);
+		// 	result = event.data;
+		// };
 
-		const mod = await import('$lib/wasm/pkg/wasm');
-		console.log(mod);
+		// const mod = await import('$lib/wasm/pkg/wasm');
+		// console.log(mod);
+		// await mod.default();
+		// console.log(mod.fibonacci(30));
+
+		const mod = await import('$lib/rustPython/pkg/rustpython_wasm');
 		await mod.default();
-		console.log(mod.fibonacci(30));
+		console.log('Loaded', new Date());
+		mod.pyEval('print(1+1)', {
+			stdout: (out: string): void => {
+				console.log(`Got stdout: ${out} (JSON/escaped: ${JSON.stringify(out)})`);
+
+				results = results.concat([out]);
+				console.log('done 1', new Date(), performance.now());
+			}
+		});
+
+		mod.pyEval('print(2+2)', {
+			stdout: (out: string): void => {
+				console.log(`Got stdout: ${out} (JSON/escaped: ${JSON.stringify(out)})`);
+
+				results = results.concat([out]);
+				console.log('done 1', new Date(), performance.now());
+			}
+		});
+
+		pyEval = mod.pyExec; // mod.pyEval // Nope: truly a single-line req
+		// mod.pyExec()
 	});
 
-	const handleFib = (num: number) => {
-		worker.postMessage(num);
+	// const handleFib = (num: number) => {
+	// 	worker.postMessage(num);
+	// };
+
+	const runPyEval = (code: string): void => {
+		if (!pyEval) {
+			alert('Not loaded!');
+			return;
+		}
+
+		pyEval(code, {
+			stdout: (out: string): void => {
+				console.log(`Got stdout: ${out} (JSON/escaped: ${JSON.stringify(out)})`);
+
+				results = results.concat([out]);
+			}
+		});
+		console.log('belo');
 	};
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>{PROJECT_NAME}</title>
 </svelte:head>
 
 <section class="section">
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
 	<div class="container">
-		<!-- <input type="number" />	 -->
-		<button class="button" on:click={() => handleFib(4)}>Run it</button>
+		<h1 class="title is-1">pysvelte</h1>
 
-		{#if result}
+		<!-- <input type="number" />	 -->
+		<!-- <button class="button" on:click={() => handleFib(4)}>Run it</button> -->
+
+		<!-- {#if result}
 			<div>Result is: {result}</div>
-		{/if}
+		{/if} -->
+		<form class="form" on:submit|preventDefault={() => runPyEval(code)}>
+			<div class="field">
+				<label class="label" for="py-eval">Python code</label>
+				<div class="control">
+					<textarea
+						class="textarea"
+						bind:value={code}
+						placeholder="Write python code here..."
+						name="py-eval"
+					/>
+				</div>
+			</div>
+			<div class="control">
+				<button class="button" disabled={!pyEval}>🏃 Run code</button>
+			</div>
+		</form>
+		<hr />
+		<h2 class="subtitle is-2">Results</h2>
+		<div>
+			{#each results as r}
+				<div class="results">
+					<code>
+						>>> {r}
+						<!-- <pre>{r}</pre> -->
+					</code>
+				</div>
+			{/each}
+		</div>
 	</div>
 </section>
+
+<style lang="scss">
+	.results {
+		width: 100%;
+		background-color: var(--code-bg-color);
+		padding-bottom: 3px;
+	}
+</style>
