51ace3f4e7fbd5250011eca12381220acd612971
Michael Wooley <wm.wooley@gmail.com>
2022-04-26 05:28:45 -0400

    lay out /meet routes

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -4,4 +4,5 @@ export const BASE = `${_BASE}/`;
 export const ABOUT = `${_BASE}/about`;
 
 export const MEET = `${_BASE}/meet`;
+export const MEET_CREATE = `${MEET}/create`;
 export const MEET_SLUG = (s: string): string => `${MEET}/${s}`;
---
+++ src/routes/meet/create.svelte
@@ -0,0 +1,3 @@
+<h1 class="title is-1">Create a new meeting</h1>
+
+<p>TODO: Create a new meeting here</p>
