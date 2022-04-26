aae8267aa9e43b78f0d3d50726079677f2984b76
Michael Wooley <wm.wooley@gmail.com>
2022-04-26 05:18:35 -0400

    init settings page

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -2,3 +2,7 @@ const _BASE = '';
 
 export const BASE = `${_BASE}/`;
 export const ABOUT = `${_BASE}/about`;
+
+export const MEET = `${_BASE}/meet`;
+export const MEET_SLUG = (s: string): string => `${MEET}/${s}`;
+export const MEET_SETTINGS_SLUG = (s: string): string => `${MEET_SLUG(s)}/settings`;
