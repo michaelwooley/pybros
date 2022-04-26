5d72e0bfc491e19429b1e11ae9dd994cba0bb5b2
Michael Wooley <wm.wooley@gmail.com>
2022-04-26 05:19:07 -0400

    remove meeting settings page

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -5,4 +5,3 @@ export const ABOUT = `${_BASE}/about`;
 
 export const MEET = `${_BASE}/meet`;
 export const MEET_SLUG = (s: string): string => `${MEET}/${s}`;
-export const MEET_SETTINGS_SLUG = (s: string): string => `${MEET_SLUG(s)}/settings`;
